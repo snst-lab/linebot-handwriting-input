8a2697b3378072cc18415d6bea0f5e477ab03e29
snst-lab <snst.lab@gmail.com>
2018-10-03 21:17:52 +0900

    update

---
+++ deploy.bat
@@ -0,0 +1,6 @@
+git config user.name "snst-lab"
+git remote rm origin
+git remote add origin git@github.com:snst-lab/linebot-handwriting-input.git
+git add .
+git commit -a -m "update"
+git push origin master
---
+++ fix.bat
@@ -0,0 +1,1 @@
+git pull git@github.com:snst-lab/linebot-handwriting-input.git master --allow-unrelated-histories
